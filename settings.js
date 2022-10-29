fc865d620d3d05026e3c649e19f3b8f19e287ebb
dontfor <116479421+dontfor@users.noreply.github.com>
2022-10-29 13:19:27 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -32,12 +32,12 @@ global.APIKeys = {
 }
 
 //other
-global.owner = ['94715166712'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
-global.pemilik = ['94715166712'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
-global.premium = ['94715166712'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
-global.pengguna = 'Dark Maker' // Your name ඔබේ නම 
+global.owner = ['94712053971'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
+global.pemilik = ['94712053971'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
+global.premium = ['94712053971'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
+global.pengguna = 'Ushira Paranga' // Your name ඔබේ නම 
 global.botnma = '💃 𝐐𝐮𝐞𝐞𝐧 𝐄𝐥𝐢𝐬𝐚 𝐁𝐨𝐭 💃' // Your bot name , ඔබගෙ බොට්ගේ නම 
-global.ownernma = '𝓓𝓪𝓻𝓴 𝓜𝓪𝓴𝓮𝓻' //ownernama,ownername
+global.ownernma = '𝓤𝓢𝓗𝓘𝓡𝓐 𝓟𝓐𝓡𝓐𝓝𝓖𝓐' //ownernama,ownername
 global.packname = 'Queen Elisa Sticker' // Sticker package name 
 global.author = 'Dark Maker' // Sticker Autor name 
 global.sessionName = 'session'
